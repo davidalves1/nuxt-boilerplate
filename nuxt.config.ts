0ce391f00fcb00bb032b970aebc39be74813c8ac
davidalves1 <alves.david@outlook.com>
2024-08-15 21:40:12 +0100

    chore: move nuxt project to src directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// eslint-disable-next-line no-undef
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
+  srcDir: 'src/',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
 });
